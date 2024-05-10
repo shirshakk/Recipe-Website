e27f5b0fea60777a16d7e34278bd9f5901f891ce
shirshakk <kshirshak76@gmail.com>
2024-05-10 19:16:05 +0530

    Requirement part pending

--- Recipe.js
+++ Recipe.js
@@ -9,16 +9,15 @@ const RecipeUrl=`https://api.spoonacular.com/recipes/${recipeId}/analyzedInstruc
 
 document.getElementById("Foodname").innerHTML=Name;
 document.getElementById("Foodimage").setAttribute("src",recipeImage);
-const RightContent=document.getElementById("Ingredient");
-
-
+const RightContent=document.getElementById("Steps");
+const LeftContentingredient=document.getElementById("IngredientText");
+const LeftContentRequirement=document.getElementById("RequirementText");
 
 
 async function getRecipe(){
     const RecipeResources=await fetch(`${RecipeUrl}?apiKey=${key}`);
     const RecipeData= await RecipeResources.json();
     const STEPS= RecipeData[0].steps;
-    console.log(STEPS);
     for(let i=0; i<STEPS.length;i++)
         {   
             const step=document.createElement("h2");
@@ -27,6 +26,27 @@ async function getRecipe(){
             const para=document.createElement("p");
             para.textContent=`${STEPS[i].step}`
             RightContent.appendChild(para)
+            const IngredientLength=RecipeData[0].steps[i].ingredients.length;
+            for(let j=0;j<IngredientLength;j++){
+                const Ingredient=RecipeData[0].steps[i].ingredients[j].name;
+                const IngredientText=document.createElement("p");
+                IngredientText.textContent=Ingredient;
+                LeftContentingredient.appendChild(IngredientText);
+            }
+            const Requirementlength=RecipeData[0].steps[i].equipment.length;
+            for(let k=0;k<Requirementlength;k++)
+                {
+                    const RequirementText=RecipeData[0].steps[i].equipment[k].name;
+                    if(RequirementText!==" ")
+                        {
+                            console.log(RequirementText);
+                            // const text=document.createElement("p");
+                            // text.textContent=RequirementText;
+                            // LeftContentRequirement.appendChild(text);
+                            
+                        }
+                }
+            
         }
 }
 
