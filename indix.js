b69fcfe595080d82b9b0bb642516d1c5bdaa1f29
shirshakk <kshirshak76@gmail.com>
2024-05-10 11:55:10 +0530

    Search Area Complete

--- indix.js
+++ indix.js
@@ -1,5 +1,5 @@
 const SearchUrl = "https://api.spoonacular.com/recipes/complexSearch";
-const Key = "242535a8bd4349f6a087851a6ab842d1";
+const Key = "28a790e2c2e74e54b5e10803bc374b51";
 const mealTypes = ["Caregory", "main course", "side dish", "dessert", "appetizer", "salad", "bread", "breakfast", "soup", "beverage", "sauce", "marinade", "fingerfood", "snack", "drink"];
 
 const dropdownOptions = mealTypes.map((mealType) => {
@@ -21,38 +21,48 @@ dropdown.addEventListener("change", function () {
     Search(selectedMealType);
 });
 async function Search(food) {
-    console.log(food);
-    const IngredientsUrl=`https://api.spoonacular.com/recipes/${716429}/information`;
-    const IngredientsResources=await fetch(`${IngredientsUrl}?apiKey=${Key}`);
-    const IngredientsData= await IngredientsResources.json();
-    console.log(IngredientsData);
-    const SearchResources = await fetch(`${SearchUrl}?apiKey=${Key}&type=${food}`);
-    const SearchData = await SearchResources.json();
+    
     const Category = document.querySelector('.Category');
-    Category.innerHTML = ''; 
-    var NewRow;
-    SearchData.results.forEach((recipe, index) => {
+    Category.innerHTML = '';
+    var InputText=document.getElementById("InputText").value.trim();  
+    let SearchEngine;
+    let NewRow;
+    if(InputText){
+        const SearchSystem = await  fetch(`${SearchUrl}?apiKey=${Key}&query=${InputText}`);
+        SearchEngine = await SearchSystem.json();
+        
+    }  
+    else{
+        const SearchResources = await fetch(`${SearchUrl}?apiKey=${Key}&type=${food}`);
+        SearchEngine = await SearchResources.json();
+        
+    }
+    SearchEngine.results.forEach((recipe, index) => {
         if(index%4===0)
             {
                 NewRow=document.createElement("div");
                 NewRow.classList.add("row");
                 Category.appendChild(NewRow);
             }
-        const card = document.createElement("div");
-        card.classList.add("card");
-        const title = document.createElement("h2");
-        title.textContent = recipe.title;   
-        const image=document.createElement("img");
-        image.src=recipe.image;
-        const getRecipe=document.createElement("button");
-        getRecipe.classList.add("btn");
-        getRecipe.textContent="Get Recipe";
-        card.appendChild(title);
-        card.appendChild(image);
-        card.appendChild(getRecipe);
-        NewRow.appendChild(card);
-    });
+            const card = document.createElement("div");
+            card.classList.add("card");
+            const title = document.createElement("h2");
+            title.textContent = recipe.title;   
+            const image=document.createElement("img");
+            image.src=recipe.image;
+            const getRecipe=document.createElement("button");
+            getRecipe.classList.add("btn");
+            getRecipe.setAttribute("data-recipe-id",recipe.id);
+            getRecipe.textContent="Get Recipe";
+            card.appendChild(title);
+            card.appendChild(image);
+            card.appendChild(getRecipe);
+            NewRow.appendChild(card);
+            console.log(food);
+        });
+        
+    
 }
-window.addEventListener("load", function(){
+    window.addEventListener("load", function(){
     Search("main course")
-  })
+})
